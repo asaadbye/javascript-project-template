08289264e39d3c205d1002751e17dcb1431948c0
petey33 <psterner@gmail.com>
2023-09-23 09:15:04 +0200

    Opdateret eslintrc.js

--- src/mysketch.js
+++ src/mysketch.js
@@ -8,6 +8,8 @@ function draw () {
   textAlign(CENTER, CENTER)
   textSize(32)
   text('Velkommen til Programmering', width / 2, height / 2)
+  rectMode(CENTER)
+  rect(width / 2, height / 2, 100, 100)
 }
 
 export { setup, draw }
