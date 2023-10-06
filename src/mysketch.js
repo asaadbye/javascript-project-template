dafac74539d5865e69fe4302a23ceab1391e7cfc
petey33 <psterner@gmail.com>
2023-10-06 09:26:51 +0200

    Mindre rettelser.

--- src/mysketch.js
+++ src/mysketch.js
@@ -8,8 +8,6 @@ function draw () {
   textAlign(CENTER, CENTER)
   textSize(32)
   text('Velkommen til Programmering', width / 2, height / 2)
-  rectMode(CENTER)
-  rect(width / 2, height / 2, 100, 100)
 }
 
 export { setup, draw }
